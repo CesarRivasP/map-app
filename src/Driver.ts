cfe4ee9a164e98ad8ef4d8360c87b3e5583b561e
cesarrivas10 <cesarjose58@gmail.com>
2020-08-30 14:11:59 -0400

    Create and implemented new class

---
+++ src/Driver.ts
@@ -0,0 +1,14 @@
+import Faker from 'faker';
+import { Person } from './Person';
+
+export class Driver extends Person {
+  constructor(){
+    super(  // SUper nos permite acceder al constructor de la clase base
+      Faker.name.findName(),
+      {
+        lat: Faker.address.latitude(),
+        long: Faker.address.longitude()
+      }
+    );
+  }
+}
