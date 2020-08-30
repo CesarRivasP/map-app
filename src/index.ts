9468b199610d09ecc10cb9f3470d0e49193acb82
cesarrivas10 <cesarjose58@gmail.com>
2020-08-30 15:03:18 -0400

    Implemented markers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { Map } from './Map';
+import { Driver } from './Driver';
+import { Passenger } from './Passenger';
 
 let map = new Map('map');
 
+const drive = new Driver();
+const passenger = new Passenger();
+
+map.handleAddDriverMarker(drive);
+map.handleAddPassengerMarker(passenger);
