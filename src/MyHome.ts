b72cc0544bebbd448c735c00b65a63f2261a1ac1
cesarrivas10 <cesarjose58@gmail.com>
2020-08-31 16:52:12 -0400

    Comment without using declarations (not secure)

--- src/MyHome.ts
+++ src/MyHome.ts
@@ -2,7 +2,7 @@ import { Map } from "./Map";
 import { Person } from "./Person";
 
 export class MyHome implements Mappable {
-  private name: string;
+  // private name: string;
   private location: {
     lat: string;
     long: string;
@@ -14,14 +14,14 @@ export class MyHome implements Mappable {
       long: (parseInt(passenger.handleGetLocation.long) + 0.0004).toString()
     };
 
-    this.name = passenger.handleGetUserName;
+    // this.name = passenger.handleGetUserName;
   }
 
   get handleGetLocation(){
     return this.location;
   }
 
-  handleGetMarkerTitle = (address: () => string): string =>  {
+  /* handleGetMarkerTitle = (address: () => string): string =>  {
     return `La direccion de ${this.name} es ${address()}`;
-  }
+  } */
 }
