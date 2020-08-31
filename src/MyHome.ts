23958f3ed6027e373a36079eb9b66a2e6f6001bb
cesarrivas10 <cesarjose58@gmail.com>
2020-08-31 15:05:28 -0400

    Implemented new function for handing marker title

--- src/MyHome.ts
+++ src/MyHome.ts
@@ -2,6 +2,7 @@ import { Map } from "./Map";
 import { Person } from "./Person";
 
 export class MyHome implements Mappable {
+  private name: string;
   private location: {
     lat: string;
     long: string;
@@ -11,10 +12,16 @@ export class MyHome implements Mappable {
     this.location = {
       lat: (parseInt(passenger.handleGetLocation.lat) + 0.0002).toString(),
       long: (parseInt(passenger.handleGetLocation.long) + 0.0004).toString()
-    }
+    };
+
+    this.name = passenger.handleGetUserName;
   }
 
   get handleGetLocation(){
     return this.location;
+  }
+
+  handleGetMarkerTitle = (address: () => string): string =>  {
+    return `La direccion de ${this.name} es ${address()}`;
   }
 }
