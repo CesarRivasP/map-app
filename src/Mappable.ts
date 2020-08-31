d356ed177c91136718bee1ae2bb3caa2e9b42450
cesarrivas10 <cesarjose58@gmail.com>
2020-08-31 16:49:59 -0400

    Add function to interface

--- src/Mappable.ts
+++ src/Mappable.ts
@@ -2,6 +2,6 @@ interface Mappable {
   handleGetLocation: {  //tiene que llamarse igual que el getter en la clase en la que se va a implementar
     lat: string,
     long: string
-  },
-  handleGetMarkerTitle(address: () => string): string
+  };
+  handleGetMarkerTitle(address: () => string): string;
 };
