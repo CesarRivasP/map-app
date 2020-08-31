b9dc3557b4fc84240b7c18cada72adab0b2ab325
cesarrivas10 <cesarjose58@gmail.com>
2020-08-30 20:51:26 -0400

    IMplemented new instance of MyHome clase

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Map } from './Map';
 import { Driver } from './Driver';
 import { Passenger } from './Passenger';
+import { MyHome } from './MyHome';
 
 let map = new Map('map');
 
@@ -14,5 +15,7 @@ map.handleAddPassengerMarker(passenger);
 // Segunda solucion para los marcadores
 const drive = new Driver();
 const passenger = new Passenger();
+const myHome = new MyHome(passenger);
 map.handleAddMarker(drive);
 map.handleAddMarker(passenger); 
+map.handleAddMarker(myHome); 
